01326b9ecac9656444eb817e22c841ea5ea8ccf8
osawa-koki <github@osawa.dev>
2023-02-20 22:17:15 +0900

    小さな修正。

--- app/display.rb
+++ app/display.rb
@@ -1,8 +1,13 @@
 
+require_relative 'is_array_sorted'
+
 def display(title, array)
+  is_sorted = is_array_sorted(array)
+  sorted_comment = is_sorted ? "\033[34mtrue \033[0m" : "\033[31mfalse\033[0m"
+
   # タイトルを右詰めで20桁にする
   title = title.ljust(20)
 
   # タイトルと配列の要素を出力する
-  puts "#{title}: #{array.join(' ')}"
+  puts "#{title} (#{sorted_comment}): #{array.join(' ')}"
 end
