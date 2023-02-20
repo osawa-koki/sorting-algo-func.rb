c2985dbb1cb670df4bb67e03b8c0f1b4755fbb82
osawa-koki <github@osawa.dev>
2023-02-20 21:40:24 +0900

    Shuffle

--- app/main.rb
+++ app/main.rb
@@ -1,7 +1,11 @@
 
 require_relative 'display'
+require_relative 'shuffle'
 
 nums = Array.new(10) { |i| i }
 
 # 配列を表示する
 Display('original', nums)
+
+Shuffle(nums)
+Display('shuffled', nums)
---
+++ app/shuffle.rb
@@ -0,0 +1,8 @@
+
+def Shuffle(array)
+  # 配列をシャッフルする
+  array.shuffle!
+
+  # シャッフルした結果の配列を返す
+  return array
+end
