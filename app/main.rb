08963914882ed97d613dc7554db09cde74d7f63e
osawa-koki <github@osawa.dev>
2023-02-20 21:43:51 +0900

    selection sort

--- app/main.rb
+++ app/main.rb
@@ -2,6 +2,7 @@
 require_relative 'display'
 require_relative 'shuffle'
 require_relative 'bubble_sort'
+require_relative 'selection_sort'
 
 nums = Array.new(10) { |i| i }
 
@@ -12,3 +13,9 @@ display('shuffled', nums)
 
 bubble_sort(nums)
 display('bubble sort', nums)
+
+shuffle(nums)
+display('shuffled', nums)
+
+selection_sort(nums)
+display('selection sort', nums)
